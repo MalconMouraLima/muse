383b74fda5f303e2ad53b828efcb3886baa59fd0
ruby -v <malcon.moura.lima@gmail.com>
2017-04-05 00:46:57 -0300

    20170405_0046

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,11 +5,11 @@ class CommentsController < ApplicationController
     @comment = Comment.create(params[:comment].permit(:content))
     @comment.user_id = current_user.id
     @comment.post_id = @post.id
-  end
 
-  if @comment.save
-    redirect_to post_path(@post)
-  else
-    render 'new'
-  end
+    if @comment.save
+      redirect_to post_path(@post)
+    else
+      render 'new'
+    end
+  end  
 end
